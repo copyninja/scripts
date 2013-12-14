034cc8e66957b9526421e4ccc93dfd0e53ec87df
Vasudev Kamath <kamathvasudev@gmail.com>
2013-12-14 14:07:10 +0530

    instead of using ar t output, just walk the extracted tree using dir command

--- check-copyright.sh
+++ check-copyright.sh
@@ -46,7 +46,7 @@ extract_archive(){
     local package=$(basename "$archive")
     unset IFS
     ar xv "$package" >/dev/null 2>&1 || exit 2
-    for file in $(ar t "$package"); do
+    for file in $(dir --hide="*.deb" "$tdir"); do
         if [ "$file" != "debian-binary" ] ; then
             dirpart=$(echo "$file" | sed 's/\.tar.*//')
             mkdir "$dirpart"
