ed72ab8339e69b48d10ea2e97a629d815a6491f2
Vasudev Kamath <kamathvasudev@gmail.com>
2014-03-01 13:28:45 +0530

    Revert "remove quoting around $(..) its protected by all shells without quoting"
    This reverts commit 58df4e47279ca5a09e594fd766a1781293beb4fe.

--- check-copyright.sh
+++ check-copyright.sh
@@ -41,15 +41,15 @@ is_valid_file(){
 # directory.
 extract_archive(){
     local archive="$1"
-    local package=$(basename "$archive")
+    local package="$(basename "$archive")"
     ar xv "$package" >/dev/null 2>&1 || exit 2
     for extracted_file in $(find "$tdir" -type f \( ! -iname "*.deb" \) -ls |\
 			     awk '{print $11}'); do
-	file=$(basename "$extracted_file")
+	file="$(basename "$extracted_file")"
         if [ "$file" != "debian-binary" ] ; then
             dirpart=$(echo "$file" | sed 's/\.tar.*//')
             mkdir "$dirpart"
-            tar -C "$dirpart" -xaf $(echo "$file")
+            tar -C "$dirpart" -xaf "$(echo "$file")"
         fi
     done
 }
